ad7c23adf6700db82741d9c0b3b79dfb7612047a
GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
2024-04-26 17:01:59 -0300

    carrinho melhorado o estilo

--- src/components/produtos/modalProduto/ModalCartProduto.tsx
+++ src/components/produtos/modalProduto/ModalCartProduto.tsx
@@ -8,7 +8,7 @@ function ModalCartProduto() {
                 <div className="items-center grid justify-center ">
 
                     <button onClick={() => setShow(!show)}>
-                    <p className="flex h-1 w-1 items-center justify-center rounded-full bg-red-500 p-2 text-xs text-white ml-5">3</p>
+                    <p className="flex h-1 w-1 items-center justify-center rounded-full bg-red-500 p-2 text-xs text-white ml-5">2</p>
                     <svg color="white" xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 " stroke-width="2" stroke="currentColor" className="file:  h-6 w-6">
     <path stroke-linecap="round" stroke-linejoin="round"
     d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
@@ -27,113 +27,96 @@ function ModalCartProduto() {
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                             <polyline points="15 6 9 12 15 18" />
                                         </svg>
-                                        <p className="text-sm pl-2 leading-none">Back</p>
+                                        <p className="text-sm pl-2 leading-none">Voltar</p>
                                     </div>
-                                    <p className="text-5xl font-black leading-10 text-gray-800 pt-3">Bag</p>
-                                    <div className="md:flex items-center mt-14 py-8 border-t border-gray-200">
-                                        <div className="w-1/4">
-                                            <img src="https://cdn.tuk.dev/assets/templates/e-commerce-kit/bestSeller3.png" className="w-full h-full object-center object-cover" />
-                                        </div>
-                                        <div className="md:pl-3 md:w-3/4">
-                                            <p className="text-xs leading-3 text-gray-800 md:pt-0 pt-4">RF293</p>
-                                            <div className="flex items-center justify-between w-full pt-1">
-                                                <p className="text-base font-black leading-none text-gray-800">North wolf bag</p>
-                                                <select className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none">
-                                                    <option>01</option>
-                                                    <option>02</option>
-                                                    <option>03</option>
-                                                </select>
-                                            </div>
-                                            <p className="text-xs leading-3 text-gray-600 pt-2">Height: 10 inches</p>
-                                            <p className="text-xs leading-3 text-gray-600 py-4">Color: Black</p>
-                                            <p className="w-96 text-xs leading-3 text-gray-600">Composition: 100% calf leather</p>
-                                            <div className="flex items-center justify-between pt-5 pr-6">
-                                                <div className="flex itemms-center">
-                                                    <p className="text-xs leading-3 underline text-gray-800 cursor-pointer">Add to favorites</p>
-                                                    <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer">Remove</p>
-                                                </div>
-                                                <p className="text-base font-black leading-none text-gray-800">$9,000</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="md:flex items-center py-8 border-t border-gray-200">
-                                        <div className="w-1/4">
-                                            <img src="https://cdn.tuk.dev/assets/templates/e-commerce-kit/bestSeller2.png" className="w-full h-full object-center object-cover" />
+                                    <p className="text-5xl font-black leading-10 text-green-800 pt-3">Carrinho</p>
+                                    <div className="md:flex items-center mt-14 py-8 border-t border-green-200">
+                                    <div className="h-full w-1/3">
+                                            <img src="https://images.pexels.com/photos/533280/pexels-photo-533280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="rounded-lg w-full h-full object-center object-cover" />
                                         </div>
                                         <div className="md:pl-3 md:w-3/4 w-full">
-                                            <p className="text-xs leading-3 text-gray-800 md:pt-0 pt-4">RF293</p>
                                             <div className="flex items-center justify-between w-full pt-1">
-                                                <p className="text-base font-black leading-none text-gray-800">Luxe Signature Ring</p>
-                                                <select className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none">
-                                                    <option>01</option>
-                                                    <option>02</option>
-                                                    <option>03</option>
-                                                </select>
-                                            </div>
-                                            <p className="text-xs leading-3 text-gray-600 pt-2">Height: 10 inches</p>
-                                            <p className="text-xs leading-3 text-gray-600 py-4">Color: Black</p>
-                                            <p className="w-96 text-xs leading-3 text-gray-600">Composition: 100% calf leather</p>
-                                            <div className="flex items-center justify-between pt-5 pr-6">
-                                                <div className="flex itemms-center">
-                                                    <p className="text-xs leading-3 underline text-gray-800 cursor-pointer">Add to favorites</p>
-                                                    <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer">Remove</p>
-                                                </div>
-                                                <p className="text-base font-black leading-none text-gray-800">$9,000</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="md:flex items-center py-8 border-t border-b border-gray-200">
-                                        <div className="h-full w-1/4">
-                                            <img src="https://cdn.tuk.dev/assets/templates/e-commerce-kit/bestSeller1.png" className="w-full h-full object-center object-cover" />
-                                        </div>
-                                        <div className="md:pl-3 md:w-3/4 w-full">
-                                            <p className="text-xs leading-3 text-gray-800 md:pt-0 pt-4">RF293</p>
-                                            <div className="flex items-center justify-between w-full pt-1">
-                                                <p className="text-base font-black leading-none text-gray-800">Luxe Signature Shoes</p>
+                                                <p className="text-base font-black leading-none text-gray-800">Tomate</p>
                                                 <select className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none">
                                                     <option>01</option>
                                                     <option>02</option>
                                                     <option>03</option>
                                                     <option>04</option>
+                                                    <option>05</option>
+                                                    <option>06</option>
+                                                    <option>07</option>
+                                                    <option>08</option>
+                                                    <option>09</option>
+                                                    <option>10</option>
                                                 </select>
                                             </div>
-                                            <p className="text-xs leading-3 text-gray-600 pt-2">Height: 10 inches</p>
-                                            <p className="text-xs leading-3 text-gray-600 py-4">Color: Black</p>
-                                            <p className="w-96 text-xs leading-3 text-gray-600">Composition: 100% calf leather</p>
+                                            <p className="text-xs leading-3 text-gray-600 pt-2">Produto agrícola</p>
+
                                             <div className="flex items-center justify-between pt-5 pr-6">
                                                 <div className="flex itemms-center">
-                                                    <p className="text-xs leading-3 underline text-gray-800 cursor-pointer">Add to favorites</p>
-                                                    <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer">Remove</p>
+                                                    <p className="text-xs leading-3 underline text-gray-800 cursor-pointer">Adicionar aos favoritos</p>
+                                                    <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer">Remover</p>
                                                 </div>
-                                                <p className="text-base font-black leading-none text-gray-800">$9,000</p>
+                                                <p className="text-base font-black leading-none text-gray-800">R$ 3.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="md:flex items-center py-8 border-t border-b border-green-200">
+                                        <div className="h-full w-1/3">
+                                            <img src="https://images.pexels.com/photos/693794/pexels-photo-693794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="rounded-lg w-full h-full object-center object-cover" />
+                                        </div>
+                                        <div className="md:pl-3 md:w-3/4 w-full">
+                                            <div className="flex items-center justify-between w-full pt-1">
+                                                <p className="text-base font-black leading-none text-gray-800">Maçã Verde</p>
+                                                <select className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none">
+                                                    <option>01</option>
+                                                    <option>02</option>
+                                                    <option>03</option>
+                                                    <option>04</option>
+                                                    <option>05</option>
+                                                    <option>06</option>
+                                                    <option>07</option>
+                                                    <option>08</option>
+                                                    <option>09</option>
+                                                    <option>10</option>
+                                                </select>
+                                            </div>
+                                            <p className="text-xs leading-3 text-gray-600 pt-2">Produto agrícola</p>
+
+                                            <div className="flex items-center justify-between pt-5 pr-6">
+                                                <div className="flex itemms-center">
+                                                    <p className="text-xs leading-3 underline text-gray-800 cursor-pointer">Adicionar aos favoritos</p>
+                                                    <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer">Remover</p>
+                                                </div>
+                                                <p className="text-base font-black leading-none text-gray-800">R$ 5.50</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="xl:w-1/2 md:w-1/3 w-full bg-gray-100 h-full">
+                                <div className="xl:w-1/2 md:w-1/3 w-full bg-green-100 h-full">
                                     <div className="flex flex-col md:h-screen px-14 py-20 justify-between overflow-y-auto">
                                         <div>
-                                            <p className="text-4xl font-black leading-9 text-gray-800">Summary</p>
+                                            <p className="text-4xl font-black leading-9 text-green-800">Resumo</p>
                                             <div className="flex items-center justify-between pt-16">
                                                 <p className="text-base leading-none text-gray-800">Subtotal</p>
-                                                <p className="text-base leading-none text-gray-800">$9,000</p>
+                                                <p className="text-base leading-none text-gray-800">R$ 8.50</p>
                                             </div>
                                             <div className="flex items-center justify-between pt-5">
-                                                <p className="text-base leading-none text-gray-800">Shipping</p>
-                                                <p className="text-base leading-none text-gray-800">$30</p>
+                                                <p className="text-base leading-none text-gray-800">Frete</p>
+                                                <p className="text-base leading-none text-gray-800">R$ 5.00</p>
                                             </div>
                                             <div className="flex items-center justify-between pt-5">
-                                                <p className="text-base leading-none text-gray-800">Tax</p>
-                                                <p className="text-base leading-none text-gray-800">$35</p>
+                                                <p className="text-base leading-none text-gray-800">Taxa</p>
+                                                <p className="text-base leading-none text-gray-800">R$ 6.00</p>
                                             </div>
                                         </div>
                                         <div>
                                             <div className="flex items-center pb-6 justify-between lg:pt-5 pt-20">
                                                 <p className="text-2xl leading-normal text-gray-800">Total</p>
-                                                <p className="text-2xl font-bold leading-normal text-right text-gray-800">$10,240</p>
+                                                <p className="text-2xl font-bold leading-normal text-right text-gray-800">R$ 19.50</p>
                                             </div>
-                                            <button onClick={() => setShow(!show)} className="text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white rounded-lg hover:bg-gray-600">
-                                                Checkout
+                                            <button onClick={() => setShow(!show)} className="text-base leading-none w-full py-5 bg-green-800 border-green-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-800 text-white rounded-lg hover:bg-green-600">
+                                                Comprar
                                             </button>
                                         </div>
                                     </div>
